6cd203625050aeaf218cf770167887fafa2e880c
Kofod95 <65596095+Kofod95@users.noreply.github.com>
2021-03-11 11:47:03 +0100

    Add files via upload

---
+++ admin/includes/languages/danish/modules/security_check/session_storage.php
@@ -0,0 +1,14 @@
+<?php
+/*
+  $Id$
+
+  CE Phoenix, E-Commerce made Easy
+  https://phoenixcart.org
+
+  Copyright (c) 2021 Phoenix Cart
+
+  Released under the GNU General Public License
+*/
+
+const WARNING_SESSION_DIRECTORY_NON_EXISTENT = 'Session-mappen eksisterer ikke:  "%s". Sessions virker ikke, før denne mappe er blevet oprettet.';
+const WARNING_SESSION_DIRECTORY_NOT_WRITEABLE = 'Jeg kan ikke skrive til session-mappen:  "%s". Sessions virker ikke, før de rigtige bruger-tilladelser er sat.';
